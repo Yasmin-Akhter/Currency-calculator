674e1d02e861de933da9603af8ec3012f87e84c1
Yasmin-Akhter <yasminbd89@yahoo.com>
2022-02-18 16:00:20 +0600

    remaining balance added

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
+function getSavings() {
+    let balance = parseFloat(document.getElementById('balance').innerText);
+    const saveInput = parseFloat(document.getElementById('save-input').value);
+    let savingsAmount = parseFloat(document.getElementById('savings-amount').innerText);
+    const savings = parseFloat(balance * saveInput / 100);
+    document.getElementById('savings-amount').innerText = savings;
+    return savings;
+}
+function getnewBalance() {
+
+}
+
 function getTotalExpenses() {
     const incomeInput = parseFloat(document.getElementById('income-input').value);
     const foodInput = parseFloat(document.getElementById('food-input').value);
@@ -8,15 +20,35 @@ function getTotalExpenses() {
     document.getElementById('total-expenses').innerText = newTotalExpenses;
     return newTotalExpenses;
 }
+
 // handling calc button 
 document.getElementById('calc-btn').addEventListener('click', function () {
     // total expenses calculation 
     const newTotalExpenses = getTotalExpenses();
-    console.log(newTotalExpenses);
+
     // total balance calculation 
     const incomeInput = parseFloat(document.getElementById('income-input').value);
     let prevbalance = document.getElementById('balance').innerText;
     const balance = incomeInput - newTotalExpenses;
     document.getElementById('balance').innerText = balance;
-    console.log(balance);
+
+
+
 });
+// handling save button
+document.getElementById('save-btn').addEventListener('click', function () {
+
+    let mySavingsAmount = document.getElementById('savings-amount');
+    mySavingsAmount = getSavings();
+
+
+    let prevbalance = parseFloat(document.getElementById('balance').innerText);
+    console.log(prevbalance);
+    let remainingBalance = document.getElementById('remaining-balance');
+    remainingBalance = prevbalance - mySavingsAmount;
+    document.getElementById('remaining-balance').innerText = remainingBalance;
+
+
+
+})
+
